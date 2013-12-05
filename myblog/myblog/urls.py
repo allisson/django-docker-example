c5a2abdc7086c3bb6d7cd089ac5e6cc440047e59
Allisson Azevedo <allisson@gmail.com>
2013-12-05 10:54:49 -0300

    Substituindo o dj-static pelo static3.

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
@@ -11,3 +12,16 @@ urlpatterns = patterns(
     # blog app
     url(r'^', include('apps.blog.urls')),
 )
+
+#from django.conf import settings
+#urlpatterns += patterns(
+#    '',
+#    (
+#        r'^media/(?P<path>.*)$',
+#        'django.views.static.serve',
+#        {
+#            'document_root': settings.MEDIA_ROOT,
+#            'show_indexes': True
+#        }
+#    ),
+#)
--- myblog/myblog/wsgi.py
+++ myblog/myblog/wsgi.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 WSGI config for myblog project.
 
@@ -11,6 +12,36 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myblog.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling, MediaCling
+from django.conf import settings
 
-application = Cling(MediaCling(get_wsgi_application()))
+from static import Cling
+
+
+STATIC_ROOT = settings.STATIC_ROOT
+STATIC_URL = settings.STATIC_URL
+MEDIA_ROOT = settings.MEDIA_ROOT
+MEDIA_URL = settings.MEDIA_URL
+
+static_cling = Cling(STATIC_ROOT + '/')
+media_cling = Cling(MEDIA_ROOT + '/')
+
+
+class ServeStaticMiddleware(object):
+
+    def __init__(self, application):
+        self.application = application
+
+    def __call__(self, environ, start_response):
+        path = environ.get('PATH_INFO', '')
+        if path.startswith(STATIC_URL) and not settings.DEBUG:
+            environ['PATH_INFO'] = path.replace(STATIC_URL, '')
+            return static_cling(environ, start_response)
+        elif path.startswith(MEDIA_URL) and not settings.DEBUG:
+            environ['PATH_INFO'] = path.replace(MEDIA_URL, '')
+            return media_cling(environ, start_response)
+
+        return self.application(environ, start_response)
+
+
+application = ServeStaticMiddleware(get_wsgi_application())
+#application = get_wsgi_application()
